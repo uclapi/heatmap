c2ef862af81839e74b018bf51d558602c57298e4
Wilhelm Klopp <wil.klopp@gmail.com>
2017-06-03 13:01:23 +0100

    Add API code

--- bookings_to_geojson.py
+++ bookings_to_geojson.py
@@ -3,9 +3,25 @@ from helpers import get_points_for_bookings, convertToGeoJSON
 
 # Write your code here
 
+UCL_API_TOKEN =
 
 
+req = requests.get(
+    "https://uclapi.com/roombookings/rooms",
+    params={
+        "token": UCL_API_TOKEN
+    }
+)
 
+rooms = req.json()["rooms"]
+
+req = requests.get(
+    "https://uclapi.com/roombookings/bookings",
+    params={
+        "token": UCL_API_TOKEN,
+        "date": "20170602"
+    }
+)
 
 # Don't change lines below
 
